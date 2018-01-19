3b7f2033ddd6a1afd4495b1f998290471e9d8f93
RamSheep <carloscprojetista@gmail.com>
2018-01-19 18:00:26 -0300

    Subindo urls

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,17 +1,15 @@
+
 from django.conf.urls import include, url
 from django.urls import path
-
 from django.contrib import admin
-admin.autodiscover()
-
 import hello.views
 
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
+
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^contato/$', hello.views.contatos, name='contatos'),
+    url(r'^produto/$', hello.views.produto, name='produto'),
+    url(r'^produtos/$', hello.views.produtos_list, name='produtos_list'),
     path('admin/', admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,33 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-from .models import Greeting
-
-import requests
-
-
-# Create your views here.
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
-
-"""
-def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
 
+def contatos(request):
+    return render(request, 'contatos.html')
 
-"""
+
+def produtos_list(request):
+    return render(request, 'produtos_list.html')
+
+
+def produto(request):
+    return render(request, 'produto.html')
