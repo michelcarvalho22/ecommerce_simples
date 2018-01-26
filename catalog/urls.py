32b06c53e2169716ce219363094e733c5c312992
RamSheep <carloscprojetista@gmail.com>
2018-01-26 15:41:29 -0300

    tentando heroku db

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,6 @@
 # coding=utf-8
-from django.conf.urls import url
-from django.urls import path
 
-import catalog.views
+from django.urls import path
 from . import views
 
 app_name = 'catalog'
@@ -11,12 +9,4 @@ urlpatterns = [
    path('', views.product_list, name='product_list'),
    path('<slug:slug>/', views.category, name='category'),
    path('produto/<slug:slug>/', views.product, name='product'),
-
 ]
-
-"""
-urlpatterns = [
-   path('', catalog.views.product_list, name='product_list'),
-   url(r'^(?P<slug>[\w_-]+)/$', views.category, name='category'),
-   url(r'^produtos/(?P<slug>[\w_-]+)/$', views.product, name='product'),
-]"""
