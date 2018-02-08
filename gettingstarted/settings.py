5d6da1b88d4a0a7390a81acb483f134d4054692c
RamSheep <carloscprojetista@gmail.com>
2018-02-08 19:50:36 -0300

    carrinho

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'hello',
     'accounts',
     'catalog',
+    'checkout'
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,16 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'accounts.backends.ModelBackend',
 )
+
+# Messages
+from django.contrib.messages import constants as messages_constants
+MESSAGE_TAGS = {
+    messages_constants.DEBUG: 'debug',
+    messages_constants.INFO: 'info',
+    messages_constants.SUCCESS: 'success',
+    messages_constants.WARNING: 'warning',
+    messages_constants.ERROR: 'danger',
+}
 
 try:
     from .local_settings import *
--- hello/views.py
+++ hello/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from .forms import ContactForm
 
@@ -24,11 +25,16 @@ index = IndexView.as_view()
 
 
 def contact(request):
+
     success = False
     form = ContactForm(request.POST or None)
     if form.is_valid():
         form.send_mail()
         success = True
+
+    elif request.method == 'POST':
+        messages.error(request, 'Formulário inválido')
+
     context = {
         'form': form,
         'success': success
